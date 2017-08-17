8d9e8de1bdcbcd42e27dbe2c6288dbc5bb34d364
西安-戚鲁成 <qilucheng@netposa.com>
2017-08-17 16:55:41 +0800

    master分支，环境配置，去掉admin服务

--- apollo-assembly/src/bin/start.sh
+++ apollo-assembly/src/bin/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup ./jre/bin/java -jar npwonder-centerconf-1.0.0.jar >/dev/null 2>&1 &
+nohup ./jre/bin/java -jar apollo-assembly-0.8.0.jar >/dev/null 2>&1 &
---
+++ package.bat
@@ -0,0 +1,1 @@
+start mvn -DskipTests clean package -Pdeploy
