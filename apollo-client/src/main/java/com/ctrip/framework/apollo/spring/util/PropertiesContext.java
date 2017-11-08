e9d1ab3072ac3939f05e8c3c10c26f9d1a12adbe
zhangxiao <zhangxiao@netposa.com>
2017-11-08 14:29:03 +0800

    [修改]1.修改配置中心客户端 2.客户端升级至0.8.2

--- apollo-client/src/main/java/com/ctrip/framework/apollo/spring/util/PropertiesContext.java
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/spring/util/PropertiesContext.java
@@ -1,13 +1,30 @@
 package com.ctrip.framework.apollo.spring.util;
 
 import com.ctrip.framework.apollo.core.utils.NetposaConstant;
+import com.ctrip.framework.apollo.core.utils.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 
 public class PropertiesContext {
-
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropertiesContext.class);
+    
+    public static final String VIAS_CONFIG_CENTER_ADDRESS = "VIAS_CONFIG_CENTER_ADDRESS";
+    
     public static void setNetposaProperties(Environment environment) {
         NetposaConstant.appId = environment.getProperty("spring.application.name");
-        NetposaConstant.devMeta = environment.getProperty("spring.netposa.center-conf.url");
-        NetposaConstant.isEnable = environment.getProperty("spring.netposa.center-conf.enable",Boolean.class);
+        String url = environment.getProperty(VIAS_CONFIG_CENTER_ADDRESS);
+        if (StringUtils.isEmpty(url)) {
+            url = environment.getProperty("spring.netposa.center-conf.url");
+        }
+        NetposaConstant.devMeta = url;
+        NetposaConstant.isEnable = environment.getProperty("spring.netposa.center-conf.enable", Boolean.class);
+        
+        LOGGER.info("---- Config Center with [ appId={} , server={} , enable={}] ----",
+                NetposaConstant.appId,
+                NetposaConstant.devMeta,
+                NetposaConstant.isEnable
+        );
     }
 }
